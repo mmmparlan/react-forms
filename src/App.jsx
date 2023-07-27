be1c026fee628626183077c0ccdb91892189b55a
mmmparlan <mmparlan@gmail.com>
2023-07-27 16:45:09 -0400

    completed SignUpForm and received token

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import Authenticate from './components/Authenticate.jsx'
+import SignUpForm from './components/SignUpForm.jsx'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Authenticate />
+      <SignUpForm />
     </>
   )
 }
